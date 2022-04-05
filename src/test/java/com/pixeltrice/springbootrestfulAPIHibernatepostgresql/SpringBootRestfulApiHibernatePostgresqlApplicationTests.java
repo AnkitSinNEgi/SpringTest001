93896b32a49d53e6cfc674bd38194385c37dc24a
@AnkitSinNEgi <asn.axelor@gmail.com>
2022-04-05 22:36:05 +0530

    Initial commit for test

---
+++ src/test/java/com/pixeltrice/springbootrestfulAPIHibernatepostgresql/SpringBootRestfulApiHibernatePostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootrestfulAPIHibernatepostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulApiHibernatePostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
